e444588e1ef6a167c85805605ac909c0ae959bef
khan-shimul <khanshimul196@gmail.com>
2024-05-07 03:09:49 +0600

    designed login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,7 +4,41 @@ const Login = () => {
   return (
     <div>
       <Navbar />
-      <h2>This is Login</h2>
+      <div className="md:3/4 lg:w-1/2 mx-auto">
+        <h2 className="text-center font-bold text-3xl mt-4">This is Login</h2>
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
